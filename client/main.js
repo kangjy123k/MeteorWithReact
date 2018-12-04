e5c8f04f23c7c64f6473bfc13f41673140e306be
kangjy123k <kjy092620@gmail.com>
2018-12-04 10:51:22 +0100

    initial commit

---
+++ client/main.js
@@ -0,0 +1,12 @@
+console.log('hello world!');
+//import the react lib
+import React from 'react';
+
+//create a component
+const APP = () =>{
+    return (
+        <div>
+            React APP #2
+        </div>
+    );
+};
