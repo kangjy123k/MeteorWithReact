0f79b38f5a4c93b5a0e3a10f75054363a3f80372
kangjy123k <kjy092620@gmail.com>
2018-12-04 12:49:12 +0100

    newest version updated

--- client/main.js
+++ client/main.js
@@ -1,12 +1,19 @@
 console.log('hello world!');
 //import the react lib
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { Meteor } from 'meteor/meteor';
+import './main.html';
 
 //create a component
-const APP = () =>{
+const App = () =>{
     return (
         <div>
-            React APP #2
+            React App #2
         </div>
     );
 };
+//render this component to the screen (adapt the browser)
+Meteor.startup(() => {
+    ReactDOM.render(<App />,document.querySelector('.container'));
+});
